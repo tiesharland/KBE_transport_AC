e1758d8c870f4786bf65efc800ac6deb1e6c079f
JIJmteVerhoeff <JanIJmteVerhoeff@hotmail.com>
2025-05-02 13:40:36 +0200

    added engine file

---
+++ Propulsion/engine.py
@@ -0,0 +1,8 @@
+
+# class engine():
+#     @Attribute
+#         def power_required(self):
+#             wp, ws = calculate_optimal_point(self.s_to, self.s_landing, self.h_cr, self.V_cr, self.A)
+#             return self.mtow / wp
+
+
