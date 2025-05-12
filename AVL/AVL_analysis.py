9836bd075b92b78402383677f368a3f1a4eac471
JIJmteVerhoeff <JanIJmteVerhoeff@hotmail.com>
2025-05-12 16:16:47 +0200

    Added ruledshell wing

--- AVL/AVL_analysis.py
+++ AVL/AVL_analysis.py
@@ -3,20 +3,43 @@ from parapy.geom import *
 import kbeutils.avl as avl
 from Wing.wing import Wing
 from Wing.Sizing import calculate_optimal_point
+from Wing.airfoil import Airfoil
+
 
 class AVL(GeomBase):
-    sections = Input()
-    root_airfoil = Input()
-    tip_airfoil = Input()
-    tip_mirrored = Input()
+    airfoil_name_root = Input()
+    root_chord = Input()
+    airfoil_name_tip = Input()
+    tip_chord = Input()
+    tip_le_offset = Input()
     surface = Input()
     span = Input()
     MAC = Input()
     mach = Input()
+    cl_cr = Input()
 
     @Part
-    def sections(self):
-        return [self.root_airfoil, self.tip_airfoil]
+    def root_airfoil(self):
+        return Airfoil(airfoil_name=self.airfoil_name_root, chord=self.root_chord, position=self.position)
+
+    @Part
+    def tip_airfoil(self):
+        return Airfoil(airfoil_name=self.airfoil_name_tip, chord=self.tip_chord,
+                       position=self.position.translate(x=self.tip_le_offset, y=self.span / 2))
+
+
+    @Part
+    def avl_section_root(self):
+        return avl.Section(chord=self.root_chord,
+                           airfoil=avl.NacaAirfoil(designation=self.airfoil_name_root),
+                           position=self.root_airfoil.position)
+
+    @Part
+    def avl_section_tip(self):
+        return avl.Section(chord=self.tip_chord,
+                           airfoil=avl.NacaAirfoil(designation=self.airfoil_name_tip),
+                           position=self.tip_airfoil.position)
+
 
     @Part
     def avl_surface(self):
@@ -26,76 +49,55 @@ class AVL(GeomBase):
                            n_spanwise=20,
                            span_spacing=avl.Spacing.cosine,
                            y_duplicate=self.position.point[1],
-                           sections=[section.avl_section for section in self.sections])
+                           sections=[self.avl_section_root,self.avl_section_tip])
 
     @Attribute(in_tree=True)
-    def avl_surfaces(self):  # a list of all AVL surfaces in the aircraft:
-        return self.find_children(lambda o: isinstance(o, avl.Surface))
-        # This automatically scans the product tree and collects all
-        # instances of the avl.Surface class.
-        # (if you don't know what `lambda` is doing there: that's a somewhat
-        # more advanced feature of functional programming, but it's not
-        # required knowledge for this course. Just use it as provided above,
-        # and you'll be fine. Feel free to check out the
-        # Python documentation about it, though, if you're curious.)
-
-        # Otherwise, you can of course also manually specify the surfaces you
-        # want to include, like this:
-        # return [self.wing.avl_surface, self.tail.avl_surface]
-        # (but make sure you forget no surface that needs to be included in the
-        # model!)
+    def avl_surfaces(self):
+        return list(self.find_children(lambda o: isinstance(o, avl.Surface)))
 
     @Part
     def avl_configuration(self):
-        return avl.Configuration(name='cruise analysis',
-                                 reference_surface=self.wing.surface,
-                                 reference_span=self.wing.span,
-                                 reference_chord=self.wing.MAC,
+        return avl.Configuration(name='cruise',
+                                 reference_area=self.surface,
+                                 reference_span=self.span,
+                                 reference_chord=self.MAC,
                                  reference_point=self.position.point,
                                  surfaces=self.avl_surfaces,
                                  mach=self.mach)
 
+
     @Attribute
     def avl_settings(self):
-        """
-        Format for AVL inputs:
-            dict(<parameter to define>: <value>)
-            value can be defined either by a number or with `avl.Parameter`:
-            avl.Parameter(name=<var to adjust>,
-                          setting=<var for which to achieve a given value>
-                          value=<value to achieve>)
-        These need to be defined either in Input or in a separate Attribute, in
-        order to allow ParaPy to trace dependencies (defining this dictionary
-        in an argument for avl.Interface() or avl.Case fails for that reason)
-        """
         return {'alpha': avl.Parameter(name='alpha',
                                        setting='CL',
                                        value=self.cl_cr)}
 
     @Part
     def avl_case(self):
-        """avl case definition using the avl_settings dictionary defined above"""
-        return avl.Case(name='fixed_cl',  # name _must_ correspond to type of case
+        return avl.Case(name='fixed_cl',
+                        # name _must_ correspond to type of case
                         settings=self.avl_settings)
+
+
 
     @Part
     def avl_analysis(self):
         return avl.Interface(configuration=self.avl_configuration,
                              # note: AVL always expects a list of cases!
-                             cases=[self.avl_case])
+                             cases=[self.avl_case]
+                             )
+
 
     @Attribute
     def l_over_d(self):
-        """process AVL results and compute L/D"""
-        # Since AVL always receives a list of cases, but produces a dictionary of
-        # results (using the name supplied to avl.Case as key)
-        # each result is itself a nested dictionary, containing a lot of
-        # information so it's a good idea to extract the relevant numbers
-        # as @Attributes
         return {result['Name']: result['Totals']['CLtot'] / result['Totals']['CDtot']
                 for case_name, result in self.avl_analysis.results.items()}
-        # The above is a bit more complicated than needed since there's only
-        # a single case name etc., but addressing it "by name" means we'd need
-        # to update the definition above every time we change something in the
-        # analysis.
 
+
+if __name__ == '__main__':
+    from parapy.gui import display
+
+
+    obj = AVL(airfoil_name_root='64318',root_chord=5, airfoil_name_tip = '64412',tip_chord=2,tip_le_offset=3,surface=20,span=5,MAC=1.5,mach=0.491,cl_cr=0.4)
+
+    display(obj)
